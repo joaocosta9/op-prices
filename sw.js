62757a7d3ba88b059095051d156a0f1ae73adf52
joaocosta9 <joaomcosta6@gmail.com>
2026-09-21 17:41:47 +0900

    prices 20260921084147

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cards-20260921080517'
+const CACHE='cards-20260921084147'
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png']
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))
